11ab0399f52e66caf812bfecf0c78ddf1d26a5c8
soumya <sdasuemk@gmail.com>
2023-06-09 16:23:54 +0530

    TD-5 feat: encryption of password and JWT token added

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,17 +1,30 @@
 const User = require('../schemas/models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const doLogin = async (req, res, next) => {
    try {
     const user = await User.findOne({userName: req.body.userName});
     console.log(user);
     if(user) {
-        if(user.password = req.body.password){
-            res.status(200).send({message: 'password matched'});
+        const isValidPassword = await bcrypt.compare(req.body.password, user.password);
+        if(isValidPassword){
+            // generate token
+            const token = jwt.sign({
+                userName: user.userName,
+                userId: user._id
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            })
+            res.status(200).json({
+                "access_token": token,
+                "message": "Login successful"
+            });
             console.log("User found successfully");
             return;
         }
     }
-    res.status(404).send("user not found");
+    res.status(404).send("Authentication failed!");
     console.log("user not found");
 
    }
--- routers/todoRouter.js
+++ routers/todoRouter.js
@@ -3,17 +3,18 @@ const addTodo = require('../middlewares/addTodo');
 const allTodos = require('../middlewares/getTodo');
 const editTodo = require('../middlewares/editTodo');
 const deleteTodo = require('../middlewares/deleteTodo');
+const authGuard = require('../middlewares/common/accessDefined');
 
 const todoRouter = express.Router();
 
 //create new
-todoRouter.post('/add', addTodo);
+todoRouter.post('/add', authGuard, addTodo);
 // read existing
-todoRouter.get('/read', allTodos);
+todoRouter.get('/read', authGuard, allTodos);
 // update existing
-todoRouter.put('/edit/:id', editTodo);
+todoRouter.put('/edit/:id', authGuard, editTodo);
 // delete existing
-todoRouter.delete('/del/:id', deleteTodo);
+todoRouter.delete('/del/:id', authGuard, deleteTodo);
 
 
 module.exports = todoRouter;
